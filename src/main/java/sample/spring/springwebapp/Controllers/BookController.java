52c2ba6eed80d19b6db8e123b78d158d74c1dd8f
jagadish <jagadish.dachepalli@gmail.com>
2022-04-01 23:54:33 +0530

    Adds thymeleaf dependency to mavn via pom.xml and also adds html files and controllers to route the requests via requestmapping

--- src/main/java/sample/spring/springwebapp/Controllers/BookController.java
+++ src/main/java/sample/spring/springwebapp/Controllers/BookController.java
@@ -1,2 +1,22 @@
-package sample.spring.springwebapp.Controllers;public class BookController {
+package sample.spring.springwebapp.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sample.spring.springwebapp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "books/list";
+    }
 }
